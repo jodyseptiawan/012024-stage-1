ebcc2177a6a9f324eb8f133e408ce6e84f6451d8
Jody Septiawan <Jody Septiawan>
2024-01-31 15:24:52 +0700

    day 6 - condition & looping

---
+++ condition.js
@@ -0,0 +1,99 @@
+// Condition
+
+// 5 == 5 => true
+// 10 == 3 => false
+
+// &&
+// ||
+// !
+
+// true
+// false
+
+// if (!(false || true || false)) {
+//   console.log("condition true");
+// } else {
+//   console.log("condition false");
+// }
+
+// if (false) {
+//   console.log("condition true");
+// } else if (true) {
+//   console.log("condition false");
+// } else if (true) {
+//   console.log("condition second false");
+// } else {
+//   console.log("condition not found");
+// }
+
+// const scoreNumber = 92;
+
+// if (scoreNumber < 40) {
+//   console.log("Score: D");
+// } else if (scoreNumber < 60) {
+//   console.log("Score: C");
+// } else if (scoreNumber < 80) {
+//   console.log("Score: B");
+// } else {
+//   console.log("Score: A");
+// }
+
+// SWITCH ============================
+
+// 0 = Minggu, 1 = Senin, ...
+
+// const day = 4;
+
+// switch (day) {
+//   case 0:
+//     console.log("Minggu");
+//     break;
+//   case 1:
+//     console.log("Senin");
+//     break;
+//   case 2:
+//     console.log("Selasa");
+//     break;
+//   case 3:
+//     console.log("Rabu");
+//     break;
+//   case 4:
+//     console.log("Kamis");
+//     break;
+//   case 5:
+//     console.log("Jumat");
+//     break;
+//   case 6:
+//     console.log("Sabtu");
+//     break;
+//   default:
+//     console.log("Tidak di temukan");
+// }
+
+// const day = "selasa";
+
+// switch (day) {
+//   case "minggu":
+//     console.log("0");
+//     break;
+//   case "senin":
+//     console.log(1);
+//     break;
+//   case "selasa":
+//     console.log(2);
+//     break;
+//   case "rabu":
+//     console.log(3);
+//     break;
+//   case "kamis":
+//     console.log(4);
+//     break;
+//   case "jumat":
+//     console.log(5);
+//     break;
+//   case "sabtu":
+//     console.log(6);
+//     break;
+//   default:
+//     console.log("Tidak di temukan");
+// }
