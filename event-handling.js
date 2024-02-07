c5191867d489266ec1beb5f62478dcd6a3e2f61a
Jody Septiawan <Jody Septiawan>
2024-02-07 15:28:14 +0800

    Day 8

---
+++ event-handling.js
@@ -0,0 +1,15 @@
+function showAlert() {
+  alert("Halo, selamat malam");
+}
+
+function showAlertDoubleClick() {
+  alert("Double Click");
+}
+
+function showAlertMouseEnter() {
+  alert("Mouse Enter");
+}
+
+function showAlertDrag() {
+  alert("Drag");
+}
